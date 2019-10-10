f8236c3463b0d1adf6a52a46c81c85f0c9dd2f25
Xrythidon <alexdanisz@live.com>
2019-10-09 20:10:47 -0400

    init commit'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import affixParser from "./handler/loop";
+import { prefix } from "./data/oneHandedSword";
+import Accordion from "./components/Accordion";
 
 function App() {
+  affixParser(prefix);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion title="ThiccBoy"></Accordion>
     </div>
   );
 }
 
 export default App;
+
+/* Random Chance, not needed for now.
+  const chance = new Chance();
+
+  const weightName = chance.weighted(affixNameArray, affixWeightArray);
+  // now match weightName to affixNameArray to get index
+  const weightNameIndex = affixNameArray.indexOf(weightName);
+
+
+  console.log(
+    chance.weighted(affixNameArray, affixWeightArray),
+    affixWeightArray[weightNameIndex],
+    (affixWeightArray[weightNameIndex] / totalAffixWeight) * 100 + "%"
+  );
+
+*/
+
+
